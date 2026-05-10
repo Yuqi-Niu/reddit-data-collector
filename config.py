bb9549fe56b896924031badd05ebd8c5c02901c1
Yuqi-Niu <s2606137@ed.ac.uk>
2026-05-10 16:25:44 +0800

    Upload config

---
+++ config.py
@@ -0,0 +1,22 @@
+CLIENT_ID = "Your_client_id"
+CLIENT_SECRET = "Your_client_secret"
+USER_AGENT = "reddit data collector by u/Yourredditusername"
+
+
+SUBREDDITS = ["privacy", "AskReddit"]
+
+POST_LIMIT = 20
+
+COLLECT_COMMENTS = True
+#COLLECT_COMMENTS = False
+
+
+COMMENT_LIMIT_PER_POST = 10
+
+SORT_TYPE = "new"
+KEYWORD = None
+#KEYWORD = "privacy"
+
+
+OUTPUT_POSTS_FILE = "reddit_posts.csv"
+OUTPUT_COMMENTS_FILE = "reddit_comments.csv"
